e4b7b3cee10fbae82d8703aff4a0b645a278a695
nofloat <304546210@qq.com>
2016-05-07 15:53:13 +0800

    redis

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -25,18 +25,7 @@ class BaseController extends Controller {
             echo json_encode($info,true);
             exit;
         }else{
-            $stunum = I('post.stuNum');
-            $idNum  = I('post.idNum');
-            $condition = array(
-                    "stuNum" => $stunum,
-                    "idNum"  => $idNum
-                );
-            $needInfo = $this->curl_init($this->apiUrl,$condition);
-            $needInfo = json_decode($needInfo,true);
-            if($needInfo['status'] != 200){
-                echo json_encode($needInfo);
-                exit;
-            }
+
             if(S($stunum) == $idNum){
 
             }else{
