3836d907b5a67da6bc99cd2ba9ae5fe1d6d86041
wupeinan <892862286@qq.com>
2016-08-19 21:18:19 +0800

    删除接口

---
+++ Application/Home/Controller/EditController.class.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class EditController extends BaseController
+{
+	protected $writor_allowed_type = array(5);
+	protected $admin_allowed_type = array(5,6);
+	protected $role;
+	/**
+	 * 删除文章
+	 * @return [type] [description]
+	 */
+	public function deleteArticle() {
+		$type_id 	= I('post.type_id');
+		$article_id = I('post.article_id');
+		$stunum		= I('post.stuNum');
+
+		//确认参数完整
+		if (empty($type_id) || empty($article_id)) {
+			$this->returnJson(801);
+			exit;
+		}
+
+		if(!in_array($type_id, $this->admin_allowed_type)) {
+			$this->returnJson(403);
+			exit;
+		}
+		$article = $this->getArticle($article_id, $type_id);
+		if($article === false) {
+			$this->returnJson(404, '', '该文章不存在');
+			exit;
+		}
+		//获取角色
+		$role = $this->getRole($article['user_id'], $stunum);
+		if (!$role){
+			exit;
+		}
+		if ($role == 'admin'  || ($role == 'writor' && in_array($type_id, $this->writor_allowed_type))) {
+			D('articles')->where('id='.$article_id)->delete();
+			$this->returnJson(200);
+		} else {
+			$this->returnJson(403);
+		}
+
+	}
+	/**
+	 * 获取文章的信息
+	 * @param  int  $article_id 文章的id值
+	 * @param  int $type_id    文章类型
+	 * @return bool|array      不存在返回false,存在返回文章数据
+	 */
+	protected function getArticle($article_id, $type_id) {
+		$position = array(
+			'id'		=> $article_id,
+			'type_id'	=> $type_id,
+			);
+		$Article = D('articles')->where($position)->find();
+		if(empty($Article)) {
+			return false;
+		}
+		return $Article;
+	}
+
+	/**
+	 * 根据status返回对应的json语句
+	 * @param  int $status 		http请求码
+	 * @param  array  $data   json里需要返回的数据
+	 * @param  string $info   重写info信息
+	 * @return [type]         [description]
+	 */
+	protected function returnJson($status, $data = array(), $info="") {
+		switch ($status) {
+			case 404: 
+				$report = array('status'=>'404', 'info'=>'请求参数错误');
+				break;
+			case 403:
+				$report = array('status'=>'403', 'info'=>'Don\'t permit');
+				break;
+			case 801:
+				$report = array('status'=>'801', 'info'=>'invalid parameter');
+				break;
+			case 200:
+				$report = array('status'=>'200', 'info'=>'success');
+				break;
+			default:
+				$report = array('status'=>$status, 'info'=>"");
+		}
+
+		if(!empty($info)) {
+			$report['info'] = $info;
+		}
+		if(!empty($data)) {
+			if(array_key_exists('info', $data) || array_key_exists('status', $data)) {
+				return false;
+			} else {
+				$report = array_merge($report, $data);
+			}
+		}
+		$json = json_encode($report);
+		echo $json;
+	}
+
+	/**
+	 * 得到该用户的角色
+	 * @param  int $article_user_id 文章作者的id
+	 * @param  int $stunum          学号
+	 * @return bool                  
+	 */
+	protected function getRole($writor_id, $stunum) {
+		if(empty($writor_id) || empty($stunum)) {
+			$this->returnJson('404');
+			return false;
+		}
+		$user = D('users')->where('stunum='.$stunum)->find();
+		$admin_exist = D('administrators')->where('user_id='.$user['id'])->find();
+		if ($admin_exist) {
+			return $role = 'admin';
+		} elseif ($writor_id == $user['id']) {
+			return $role = 'writor';
+		} else {
+			$this->returnJson('403');
+			return false;
+		}
+	}	
+}
