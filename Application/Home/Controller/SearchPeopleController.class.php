401011f59f22f5266353939d70bc2c2cfe2e9841
wupeinan <892862286@qq.com>
2016-09-05 16:03:13 +0800

    格式错误~

--- Application/Home/Controller/SearchPeopleController.class.php
+++ Application/Home/Controller/SearchPeopleController.class.php
@@ -43,7 +43,7 @@ class SearchPeopleController extends Controller {
             if (empty($data)) {
                $this->returnJson(404, '', array(), JSON_FORCE_OBJECT);
             }else{
-                $this->returnJson(200, '', $data, JSON_FORCE_OBJECT);
+                $this->returnJson(200, '', $data, true);
             }
         }
     }
