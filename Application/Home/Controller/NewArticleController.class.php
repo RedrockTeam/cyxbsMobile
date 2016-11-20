bd4b60fdb98bb04ebf751f5e78dead31ea334862
wupeinan <892862286@qq.com>
2016-11-20 20:51:04 +0800

    display topic

--- Application/Home/Controller/NewArticleController.class.php
+++ Application/Home/Controller/NewArticleController.class.php
@@ -35,22 +35,8 @@ class NewArticleController extends Controller
             $data_notice   = $notice->where($data_notice_condition)->order('created_time')->select();
             $site = $_SERVER["SERVER_NAME"];
             foreach ($data_notice as $key => $value) {
-                $praise_condition = array(
-                    "articletype_id" => "6",
-                    "article_id"      => $data_notice[$key]['id'],
-                    "stunum"          => I('post.stuNum')
-                );
                 $stuNum = I('post.stuNum');
-                if(!empty($stuNum)){
-                	$praise_exist = $praise->where($praise_condition)->find();
-                	if($praise_exist){
-                    	$exist = true;
-                	}else{
-                    	$exist = false;
-                	}
-                } else {
-                	$exist = false;
-                }
+                $exist = $this->is_my_like($value['article_id'], 6, $stuNum);
                 $now_info = array(
                     'status' => 200,
                     'page'   => $page,
@@ -89,22 +75,8 @@ class NewArticleController extends Controller
             } elseif ($data[$key]['articletype_id'] < 5) {
                 $article = M("news");
                 $praise  = M('articlepraises');
-                $praise_condition = array(
-                    "articletype_id"  => $data[$key]['articletype_id'],
-                    "article_id"      => $data[$key]['article_id'],
-                    "stunum"          => I('post.stuNum')
-                );
                 $stuNum = I('post.stuNum');
-                if(!empty($stuNum)){
-	                $praise_exist = $praise->where($praise_condition)->find();
-	                if($praise_exist){
-	                    $exist = true;
-	                }else{
-	                    $exist = false;
-	                }
-	             } else {
-	             	$exist = false;
-	             } 
+                $exist = $this->is_my_like($value['article_id'], $value['articletype_id'], $stuNum);
                 $articles = $article->where($condiion_articles)->find();
                 $now_info = array(
                     'status' => 200,
@@ -141,16 +113,7 @@ class NewArticleController extends Controller
                 );
                 
                 $stuNum = I('post.stuNum');
-                if(!empty($stuNum)){
-	                $praise_exist = $praise->where($praise_condition)->find();
-	                if($praise_exist){
-	                    $exist = true;
-	                }else{
-	                    $exist = false;
-	                }
-	            } else {
-	            	$exist = false;
-	            }
+                $value['is_my_like'] = $this->is_my_like($value['id'], $value['articletype_id'], $stuNum);
                 $photo_content = $articlePhoto->where($photo_condition)->select();
                 $now_info = array(
                     'status' => 200,
@@ -211,22 +174,8 @@ class NewArticleController extends Controller
         $praise  = M('articlepraises');
         $result = array();
         foreach($content as $key => $value){
-            $praise_condition = array(
-                "articletype_id" => $value['type_id'],
-                "article_id"      => $value['id'],
-                "stunum"          => I('post.stuNum')
-            );
             $stuNum = I('post.stuNum');
-            if(!empty($stuNum)){
-	            $praise_exist = $praise->where($praise_condition)->find();
-	            if($praise_exist){
-	                $value['is_my_like'] = true;
-	            }else{
-	                $value['is_my_like'] = false;
-	            }
-	       } else {
-	       	$value['is_my_like'] = false;
-	       }    
+            $value['is_my_like'] = $this->is_my_like($value['id'], $value['type_id'], $stuNum);
             array_push($result,$value);
         }
 
@@ -258,22 +207,8 @@ class NewArticleController extends Controller
         $praise  = M('articlepraises');
         $result = array();
         foreach($content as $key => $value) {
-            $praise_condition = array(
-                "articletype_id"  => $value['articletype_id'],
-                "article_id"      => $value['id'],
-                "stunum"          => I('post.stuNum')
-            );
             $stuNum = I('post.stuNum');
-            if(!empty($stuNum)){
-            	$praise_exist = $praise->where($praise_condition)->find();
-            	if($praise_exist){
-                	$value['is_my_like'] = true;
-            	}else{
-                	$value['is_my_like'] = false;
-            	}             
-            } else {
-            	$value['is_my_like'] = false;
-            }
+            $value['is_my_like'] = $this->is_my_like($value['id'], $value['articletype_id'], $stuNum);
             array_push($result,$value);
         }
          $info = array(
@@ -320,21 +255,7 @@ class NewArticleController extends Controller
             $mynum = I('post.stuNum');
             $praise = M('articlepraises');
             foreach ($contents as &$content) {
-                if (empty($mynum)) {
-                    $content['is_my_like'] = false;
-                } else {
-                    $position = array(
-                        'article_id'=> $content['id'],
-                        'type_id'   => $content['type_id'],
-                        'stunum'    => $mynum,
-                    );
-                    $praise_exist = $praise->where($position)->find();
-                    if($praise_exist) {
-                        $content['is_my_like'] = true;
-                    } else {
-                        $content['is_my_like'] = false;
-                    }
-                }
+                $content['is_my_like'] = $this->is_my_like($content['id'], $content['type_id'], $mynum);
             }
             $info = array(
                 'status' => '200',
@@ -349,24 +270,51 @@ class NewArticleController extends Controller
         $type_id = I('post.type_id');
         $stunum = I('post.stuNum');
         $article_id = I('post.article_id');
-        if( $article_id == null || $type_id == null){
+
+        if ( $article_id == null || $type_id == null) {
             $info = array(
                 "status" => 801,
                 "info"   => "invalid parameter"
             );
             echo json_encode($info);exit;
-        }else{
+        } else {
             $condition_user = array(
                 "stunum" => $stunum
             );
             if($type_id >= 5) {
-                $article    = D('articles');
+                $articletable = EditController::$table_type[$type_id];
+                
+                if (empty($articletable)) {
+                    returnJson(404, 'error type_id');
+                }
+
+                $article    = D($articletable);
+                //条件
                 $condition_article = array(
-                        'cyxbsmobile_articles.id'      => $article_id,
-                        'cyxbsmobile_articles.type_id' => $type_id,
-                        'cyxbsmobile_articles.state'   => array('neq', 0),
+                        $articletable.'.id'      => $article_id,
+                        $articletable.'.type_id' => $type_id,
+                        $articletable.'.state'   => array('neq', 0),
                     );
-                $content = $article->where($condition_article)->join('cyxbsmobile_users ON cyxbsmobile_articles.user_id = cyxbsmobile_users.id')->field('cyxbsmobile_articles.id,cyxbsmobile_articles.photo_src,cyxbsmobile_articles.thumbnail_src,cyxbsmobile_articles.content,cyxbsmobile_articles.type_id,cyxbsmobile_articles.updated_time,cyxbsmobile_articles.created_time,cyxbsmobile_articles.like_num,cyxbsmobile_articles.remark_num,cyxbsmobile_users.photo_src as user_photo,cyxbsmobile_users.nickname')->select();
+                //显示的参数
+                $displayField = array(
+                    $articletable.'.id',
+                    'content',
+                    'nickname',
+                    'remark_num',
+                    'like_num',
+                    $articletable.'.photo_src',
+                    $articletable.'.thumbnail_src',
+                    $articletable.'.type_id',
+                    $articletable.'.updated_time',
+                    $articletable.'.created_time',
+                    $user.'.photo_src' => 'user_photo'
+                );
+                $content = $article
+                            ->alias($articletable)
+                            ->where($condition_article)
+                            ->join('cyxbsmobile_users users ON '.$articletable.'.user_id = users.id')
+                            ->field($displayField)
+                            ->select();
             } elseif ($type_id > 0 && $type_id <5) {                  //新闻内容
                 $news = D('news');
                 $condition_news = array(
@@ -385,34 +333,11 @@ class NewArticleController extends Controller
                     );
                 $content = $news->where($condition_news)->field($field)->select();
             } else {
-                 $info = array(
-                "status" => 801,
-                "info"   => "invalid parameter"
-                 );
-            echo json_encode($info);exit;
-            }
+                 returnJson(801);
+             }
 
             foreach ($content as $key => &$value) {
-           
-                if($value){
-                    $mark = empty($stunum);
-                    if (!$mark) {
-                        $praise_condition = array(
-                            'article_id'    => $article_id,
-                            'type_id'       => $type_id,
-                            'stunum'        => $stunum
-                            );
-                        $praise = M('articlepraises');
-                        $praise_exist = $praise->where($praise_condition)->find();
-                        if ($praise_exist) {
-                            $value['is_my_like'] = true;
-                        } else {
-                            $value['is_my_like'] = false;
-                        }
-                    } else {
-                        $value['is_my_like'] = false;
-                    }
-                }
+                $value['is_my_like'] = $this->is_my_like($article_id, $type_id, $stunum);
             }
             $info = array(
                 'status' => '200',
@@ -424,18 +349,43 @@ class NewArticleController extends Controller
     }
 
     /**
+     * 判断我是否对该文章点赞
+     * @param  number  $article_id 文章的id值
+     * @param  number  $type_id    文章类型
+     * @param  string  $stunum     学号
+     * @return boolean             是否喜欢
+     */
+    public function is_my_like($article_id, $type_id, $stunum)
+    {
+        if(empty($stunum)) {
+            return false;
+        }
+        $praise_condition = array(
+            'article_id'    => $article_id,
+            'articletype_id'=> $type_id,
+            'stunum'        => $stunum
+            );
+        $praise = M('articlepraises');
+        $praise_exist = $praise->where($praise_condition)->find();
+        if ($praise_exist) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * 话题范围
      * @return [type] [description]
      */
     public function  topicList()
     {
-        $vertion = '0.0.1';
         $post = I('post.');
         $get = I('get.');
         $information = array_merge($get, $post);
         $information['page'] = isset($information['page']) ? $information['page'] : 0;
         $information['size'] = isset($information['size']) ? $information['size'] : 3;
-
+        
         $displayField = array(
             "topics.id" => "topic_id",
             "stunum" => 'user_id',
@@ -463,14 +413,86 @@ class NewArticleController extends Controller
                     ->order('created_time')
                     ->limit($information['page']*$information['size'], $information['size'])
                     ->select();
-        $foreach ($data as $key => &$value) {
+        
+        foreach ($data as $key => &$value) {
             $value['img']['img_small_src'] = $value['photo_src'];
             $value['img']['img_src'] = $value['thumbnail_src'];
+            
+            $value['is_my_like)'] = $this->is_my_like($value['id'], $value['type_id'], $information['stuNum']);
+            
             unset($value['photo_src']);
             unset($value['thumbnail_src']);
             $value['content'] = array("content" => $value['content']);
         }
-        returnJson(200, , compact($data, $vertion));
+        returnJson(200, '', compact($data, $vertion));
+    }
+
+    /**
+     * 话题文章列表
+     */
+    public function listTopicArticle()
+    {
+        //兼容post和get请求
+        $post = I('post.');
+        $get = I('get.');
+        $information = array_merge($post, $get);
+        $information['page'] = isset($information['page']) ? $information['page'] : 0;
+        $information['size'] = isset($information['size']) ? $information['size'] : 10;
+        
+        $article_alias = 'article';
+        $user_alias = 'user';
+        
+        $displayField = array(
+            $article_alias.'.id',
+            'type_id',
+            $article_alias.'.photo_src'     => 'article_photo_src',
+            $article_alias.'.thumbnail_src' => 'article_thumbnail_src',
+            'content',
+            'nickname',
+            'stunum',
+            $user_alias.'.photo_src'        => 'photo_src',
+            $user_alias.'.thumbnail_src'    => 'photo_thumbnail_src',
+            'like_num',
+            'remark_num',
+        );
+        //话题
+        $topic = M('topics')->field(true, 'state')->find($information['topic_id']);
+        //user_id为0时使用官方的身份
+        if ($topic['user_id'] == 0) {
+            $topic['nickname'] = "红岩网校工作站";
+            $topic['photo_src'] = "http://".$site.'/cyxbsMobile/Public/HONGY.jpg';
+        } else {
+            $author = M('users')->find($topic['user_id']);
+            $topic['nickname'] = $user['nickname'];
+            $topic['photo_src'] = $user['photo_src'];
+        }
+        unset($topic['user_id']);
+        if (!$topic) {
+            returnJson(404, 'error topic\'s id');
+        }
+        $pos = array('topic_id' => $topic['id'], 'state'=> 1);
+        $articles = M('topicarticles')
+                    ->alias($article_alias)
+                    ->join('__USERS__ '.$user_alias.' ON '.$user_alias.'.id='.$article_alias.'.user_id', "RIGHT")
+                    ->where($pos)
+                    ->field($displayField)
+                    ->order('updated_time DESC')
+                    ->limit($information['page']*$information['size'], $information['size'])
+                    ->select();
+
+        $praise = M('articlepraises');
+        
+        foreach ($data as $key => $value) {
+            if($value['user_id'] == 0) {
+                $value['nickname'] = "红岩网校工作站";
+                $value['photo_src'] = "http://".$site.'/cyxbsMobile/Public/HONGY.jpg';
+                $value['thumbnail_src'] = "http://".$site.'/cyxbsMobile/Public/HONGY.jpg';
+            }
+            $value['is_my_like)'] = $this->is_my_like($value['id'], $value['type_id'], $information['stuNum']);
+        }
+
+        $topic['articles'] = $articles;
+        returnJson(200, '',array('data' => $topic));
     }
 
 
