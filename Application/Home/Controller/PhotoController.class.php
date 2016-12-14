861d850dd41d93d459127087e3206d4fdc51a248
pumbf Aaron <pumbf Aaron>
2016-12-14 16:19:08 +0800

    保存路径问题

--- Application/Home/Controller/PhotoController.class.php
+++ Application/Home/Controller/PhotoController.class.php
@@ -203,10 +203,6 @@ class PhotoController extends Controller {
     protected function pictrueUpload($files='', &$error='', $is_detaild = false, $config = array())
     {
         $config = array_merge($this->config, $config);
-        echo $config['rootPath'];
-        echo realpath($config['rootPath']);
-        var_dump(is_dir($config['rootPath']));
-        var_dump(is_writable($config['rootPath']));exit;
         $upload = new \Think\Upload($config);
         
         $upload->saveName = time().'_'.mt_rand();
