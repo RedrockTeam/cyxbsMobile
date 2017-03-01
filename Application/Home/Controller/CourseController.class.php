8478fbdc6f9ccab8f1db4349c6d77af06e813dd0
AInotamm <rondonay@gmail.com>
2017-03-01 13:57:02 +0800

    fix bug

--- Application/Home/Controller/CourseController.class.php
+++ Application/Home/Controller/CourseController.class.php
@@ -437,7 +437,7 @@ class CourseController extends Controller
         $clear = array();
 
         foreach ($whitelist as $key)
-            array_push($clear, $whitelist[$key]);
+            array_push($clear, $table[$whitelist[$key]]);
 
         return $clear;
     }
