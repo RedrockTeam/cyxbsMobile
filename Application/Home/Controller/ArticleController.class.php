25a91ce9ffffddbf33e9de8effbc13e7c615e343
pumbf <892862286@qq.com>
2017-03-26 15:19:07 +0800

    将edit的功能迁移到Article

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -176,7 +176,7 @@ class ArticleController extends BaseController {
         }
         $article = Article::setArticle($data, $data['stuNum']);
         if ($article->add())
-            returnJson(200);
+            returnJson(200, '', array('state'=>200));
         else
             returnJson(404, $article->getError());
 
@@ -235,156 +235,13 @@ class ArticleController extends BaseController {
     
 
     public function searchHotArticle(){
-        $hotArticle = D("hotarticles");
-        $article = D("articles");
-        $praise  = M('articlepraises');
-        $user = D('users');
-        $page = I('post.page');
-        $size = I('post.size');
-        $page = empty($page) ? 0 : $page;
-        $size = empty($size) ? 15 : $size;
-        $start = $page*$size;
-        $info = array();
-        $now_date = date("Y-m-d H:i:s",mktime(0,0,0,date("m"),date("d")-7,date("Y")));
-        if($page == 0){
-            $notice = M('notices');
-            $data_notice_condition = array(
-                    "created_time" => array('GT',$now_date),
-                    "state"        => 1,
-                );
-            $data_notice   = $notice->where($data_notice_condition)->order('created_time')->select();
-            $site = $_SERVER["SERVER_NAME"];
-            foreach ($data_notice as $key => $value) {
-                $praise_condition = array(
-                    "articletype_id" => "6",
-                    "article_id"      => $data_notice[$key]['id'],
-                    "stunum"          => I('post.stuNum')
-                );
-                $praise_exist = $praise->where($praise_condition)->find();
-                if($praise_exist){
-                    $exist = true;
-                }else{
-                    $exist = false;
-                }
-                $now_info = array(
-                    'status' => 200,
-                    'page'   => $page,
-                    'data'   =>array(
-                                'id'        => $data_notice[$key]['id'],
-                                'type'      => "notice",
-                                'type_id'   => "6",
-                                'article_id'=> $value['id'],
-                                'user_id'   => "0",
-                                'nick_name' => "红岩网校工作站",
-                                'user_head' => "http://".$site.'/cyxbsMobile/Public/HONGY.jpg',
-                                'time'      => $value['created_time'],
-                                'content'   => array(
-                                                    "content" =>$value['content'],
-                                                ),
-                                'img'       => array(
-                                                'img_small_src' => $value['thumbnail_src'],
-                                                'img_src' => $value['photo_src'],
-                                            ),
-                                'like_num'  => $value['like_num'],
-                                'remark_num'=> $value['remark_num'],
-                                "is_my_Like"=> $exist,
-                            ),
-                );
-               array_push($info,$now_info);
-            }
-        }
-        $info = array_reverse($info);
-        $data = $hotArticle->where("created_time > '$now_date'")->order('(remark_num*2+like_num) DESC,updated_time DESC')->limit($start,$size)->relation(true)->select();
-        foreach ($data as $key => $value) {
-            $condiion_articles = array(
-                "id" => $data[$key]['article_id'],
-                );
-            if($data[$key]['Articletypes']['typename'] == null){
-
-            }elseif($data[$key]['articletype_id'] < 5){
-                $article = M("news");
-                $praise  = M('articlepraises');
-                $praise_condition = array(
-                    "articletype_id"  => $data[$key]['articletype_id'],
-                    "article_id"      => $data[$key]['article_id'],
-                    "stunum"          => I('post.stuNum')
-                );
-                $praise_exist = $praise->where($praise_condition)->find();
-                if($praise_exist){
-                    $exist = true;
-                }else{
-                    $exist = false;
-                }
-                $articles = $article->where($condiion_articles)->find();
-                $now_info = array(
-                    'status' => 200,
-                    'page'   => $page,
-                    'data'   =>array(
-                                'id'        => $data[$key]['id'],
-                                'type'      => $data[$key]['Articletypes']['typename'],
-                                'type_id'   => $data[$key]['articletype_id'],
-                                'article_id'   => $data[$key]['article_id'],
-                                'user_id'   => "",
-                                'user_name' => "",
-                                'user_head' => "",
-                                'time'      => $articles['date'],
-                                'content'   => $articles,
-                                'img'       => array(
-                                                'img_small_src' => "",
-                                                'img_src' => "",
-                                            ),
-                                'like_num'  => $value['like_num'],
-                                'remark_num'=> $value['remark_num'],
-                                "is_my_Like"=> $exist,
-                            ),
-                );
-                array_push($info,$now_info);
-            }else{
-                $article = D('articles');
-                $praise  = M('articlepraises');
-                $articlePhoto  = M('articlephoto');
-                $articles = $article->where($condiion_articles)->relation(true)->find();
-                $praise_condition = array(
-                    "articletypes_id" => $data[$key]['articletypes_id'],
-                    "article_id"      => $data[$key]['article_id'],
-                    "stunum"          => I('post.stuNum')
-                );
-                $praise_exist = $praise->where($praise_condition)->find();
-                if($praise_exist){
-                    $exist = true;
-                }else{
-                    $exist = false;
-                }
-                $photo_content = $articlePhoto->where($photo_condition)->select();
-                $now_info = array(
-                    'status' => 200,
-                    'page'   => $page,
-                    'data'   =>array(
-                                'id'        => $data[$key]['id'],
-                                'type'      => $data[$key]['Articletypes']['typename'],
-                                'type_id'   => $data[$key]['articletype_id'],
-                                'article_id'=>$data[$key]['article_id'],
-                                'user_id'   => $articles['Users']['stunum'],
-                                'nick_name' => $articles['Users']['nickname'],
-                                'user_head' => $articles['Users']['photo_src'],
-                                'time'      => $articles['created_time'],
-                                'content'   => array(
-                                                    "content" => $articles['content'],
-                                                ),
-                                'img'       => array(
-                                                'img_small_src' => $articles['thumbnail_src'],
-                                                'img_src' => $articles['photo_src'],
-                                            ),
-                                'like_num'  => $value['like_num'],
-                                'remark_num'=> $value['remark_num'],
-                                "is_my_Like"=> $exist,
-                            ),
-                );
-                array_push($info,$now_info);
-            }
-        }
-
-        echo json_encode($info);
+        $get = I('get.');
+        $post = I('post.');
+        $url = U('NewArticle/searchHotArticle', $get, true, true);
+        $result = curlPost($url, $post);
+        if ($result)
+            returnJson(404);
+        echo $result;
     }
 
     public function postNotice(){
@@ -495,6 +352,57 @@ class ArticleController extends BaseController {
     	echo json_encode($info,true);
     }
 
+    /**
+     * 恢复软删除的文章
+     */
+    public function recoverArticle()
+    {
+        $information = I('post.');
+        $article = Article::setArticle($information, $information['stuNum']);
+
+        if ($article === false)
+            returnJson(404, 'error article');
+        $result = $article->recover();
+        if ($result)    returnJson(200);
+        else returnJson(404, $article->getError());
+    }
+
+    /**
+     * 删除文章
+     * @return [type] [description]
+     */
+    public function deleteArticle()
+    {
+        $type_id 	= I('post.type_id');
+        $article_id = I('post.article_id');
+        $stuNum		= I('post.stuNum');
+        $forceDelete = I('post.forceDelete');
+        //确认参数完整
+        if (empty($type_id) || empty($article_id)) {
+            returnJson(801);
+            exit;
+        }
+        $article = compact('type_id', 'article_id');
+
+        $article =  Article::setArticle($article, $stuNum);
+
+        if ($article === false) returnJson(404, "error article");
+
+        $result = $article->delete($forceDelete);
+
+        if($result)
+            returnJson(200);
+        else
+            returnJson(404, $article->getError());
+    }
+
+    public function editArticle()
+    {
+        $information = I('post.');
+        if (empty($information['type_id']) || empty($information['article_id'])) {
+            returnJson(801);
+        }
+    }
 
 
 
