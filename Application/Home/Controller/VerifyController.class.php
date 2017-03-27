2042d16241a3efd57c51f04ed7da2f25ea997327
pumbf <892862286@qq.com>
2017-03-27 18:08:06 +0800

    win 登陆接口

--- Application/Home/Controller/VerifyController.class.php
+++ Application/Home/Controller/VerifyController.class.php
@@ -11,19 +11,23 @@ namespace Home\Controller;
 
 class VerifyController  extends BaseController
 {
+    public function _initialize() {}
     public function verifyLogin()
     {
+
         $stuNum = I('post.stuNum');
         $idNum = I('post.idNum');
         if (empty($idNum) || empty($stuNum)) {
             returnJson(801);
         }
-        if ($idNum != is_numeric($idNum)) {
+
+        if (6 != mystrlen($idNum)) {
             if (false === $idNum = $this->decrypt($idNum)) {
                 returnJson(404);
             }
         }
-        echo curlPost($this->apiUrl, I('post.'));
+
+        echo curlPost($this->apiUrl, compact('stuNum', 'idNum'));
 
     }
 }
