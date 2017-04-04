ef5747f5e1496c0cf2f9e88c783b0b4d058c561f
pumbf <892862286@qq.com>
2017-04-04 17:01:36 +0800

    多文件上传问题

--- Public/js/admin/fileUpload.js
+++ Public/js/admin/fileUpload.js
@@ -14,38 +14,7 @@ var FormFileUpload = function () {
                 acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
                 // Uncomment the following to send cross-domain cookies:
                 //xhrFields: {withCredentials: true},
-                uploadTemplateId: null,
-                downloadTemplateId: null,
                 previewMaxWidth:50,
-                uploadTemplate:' {% for (var i=0, file; file=o.files[i]; i++) { %}\
-                  <tr class="template-upload fade">\
-                      <td>\
-                          <span class="preview"></span>\
-                      </td>\
-                      <td>\
-                          <p class="name">{%=file.name.substr(0,8)%}</p>\
-                          <strong class="error text-danger"></strong>\
-                      </td>\
-                      <td>\
-                          <p class="size">加载中</p>\
-                          <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>\
-                      </td>\
-                      <td>\
-                          {% if (!i && !o.options.autoUpload) { %}\
-                              <button class="btn btn-primary start" disabled>\
-                                  <i class="glyphicon glyphicon-upload"></i>\
-                                  <span>开始</span>\
-                              </button>\
-                          {% } %}\
-                          {% if (!i) { %}\
-                              <button class="btn btn-warning cancel">\
-                                  <i class="glyphicon glyphicon-ban-circle"></i>\
-                                  <span>取消</span>\
-                              </button>\
-                          {% } %}\
-                      </td>\
-                  </tr>\
-              {% } %}',
             });
 
             // Enable iframe cross-domain access via redirect option:
